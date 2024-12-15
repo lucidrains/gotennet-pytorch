11142dfbd6258ec9f574232e7c84c24446e61a9c
lucidrains <lucidrains@gmail.com>
2024-12-14 16:50:12 -0800

    knock out the hierarchical tensor refinement module proposed in section 3.4

--- gotennet_pytorch/gotennet.py
+++ gotennet_pytorch/gotennet.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
 import torch
-from torch import nn, Tensor, einsum
 import torch.nn.functional as F
+from torch import nn, cat, stack, Tensor, einsum
 from torch.nn import Linear, Sequential, Module, ModuleList, ParameterList
 
 import einx
@@ -26,22 +26,23 @@ class EquivariantFeedForward(Module):
     def __init__(
         self,
         dim,
-        max_num_degrees,
+        max_degree,
         mlp_expansion_factor = 2.
     ):
         """
         following eq 13
         """
         super().__init__()
-        assert max_num_degrees > 0
+        assert max_degree > 1
+        self.max_degree = max_degree
 
         mlp_dim = int(mlp_expansion_factor * dim * 2)
 
-        self.projs = ParameterList([nn.Parameter(torch.randn(dim, dim)) for _ in range(max_num_degrees)])
+        self.projs = ParameterList([nn.Parameter(torch.randn(dim, dim)) for _ in range(max_degree)])
 
         self.mlps = ModuleList([
             Sequential(Linear(dim * 2, mlp_dim), nn.SiLU(), Linear(mlp_dim, dim * 2))
-            for _ in range(max_num_degrees - 1)
+            for _ in range(max_degree)
         ])
 
     def forward(
@@ -49,6 +50,7 @@ class EquivariantFeedForward(Module):
         h: Tensor,
         x: tuple[Tensor, ...]
     ):
+        assert len(x) == self.max_degree
 
         h_residual = 0.
         x_residuals = []
@@ -80,6 +82,51 @@ class EquivariantFeedForward(Module):
         x = [*map(sum, zip(x, x_residuals))]
 
         return h, x
+
+# hierarchical tensor refinement
+# section 3.4
+
+class HierarchicalTensorRefinement(Module):
+    def __init__(
+        self,
+        dim,
+        dim_edge_refinement, # they made this value much higher for MD22 task. so it is an important hparam for certain more difficult tasks
+        max_degree,
+    ):
+        super().__init__()
+        assert max_degree > 0
+
+        # in paper, queries share the same projection, but each higher degree has its own key projection
+
+        self.to_queries = nn.Parameter(torch.randn(dim, dim_edge_refinement))
+
+        self.to_keys = ParameterList([nn.Parameter(torch.randn(dim, dim_edge_refinement)) for _ in range])
+
+        # the two weight matrices
+        # one for mixing the inner product between all queries and keys across degrees above
+        # the other for refining the t_ij passed down from the layer before as a residual
+
+        self.residue_update = nn.Linear(dim, dim, bias = False)
+        self.edge_proj = nn.Linear(dim_edge_refinement, dim, bias = False)
+
+    def forward(
+        self,
+        t_ij: Tensor,
+        x: tuple[Tensor, ...],
+    ):
+        # eq (10)
+
+        queries = [einsum('... d m, ... d e -> ... e m', one_degree, self.to_queries) for one_degree in x]
+
+        keys = [einsum('... d m, ... d e -> ... e m', one_degree, to_keys) for one_degree, to_keys in zip(x, self.to_keys)]
+
+        # eq (11)
+
+        inner_product = [einsum('... i d m, ... j d m -> ... i j d', one_degree_query, one_degree_key) for one_degree_query, one_degree_key in zip(queries, keys)]
+
+        w_ij = cat(inner_product, dim = -1)
+
+        return self.edge_proj(w_ij) + self.residue_update(t_ij) # eq (12)
 
 # main class
 
